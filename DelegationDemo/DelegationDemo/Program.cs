f8718730c1c7bca3a30ec226312251a22e3f7721
adeankush3 <adeankush3@gmail.com>
2022-02-16 12:40:25 +0530

    [Ankush]Single Cast Program Complete

--- DelegationDemo/DelegationDemo/Program.cs
+++ DelegationDemo/DelegationDemo/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace DelegationDemo
 {
-    internal class Program
+    public delegate int operation(int x, int y);
+    public class Program
     {
+        static int Addition(int x,int y)
+        {
+            return x + y;   
+        }
+        
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Delection program");
+
+            Console.WriteLine("Delegation POC!");
+            operation obj=new operation(Addition);
+            Console.WriteLine("Addition is {0}", obj(17, 33));
+            Console.WriteLine();
         }
     }
 }
